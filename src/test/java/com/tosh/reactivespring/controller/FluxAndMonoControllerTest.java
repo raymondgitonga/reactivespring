cb35c2ee4e3811f2ead94c2d997be54eeec90603
raymond gitonga <raytosh95@gmail.com>
2020-07-13 17:54:48 +0300

    Create non blocking api using annotated controllers

---
+++ src/test/java/com/tosh/reactivespring/controller/FluxAndMonoControllerTest.java
@@ -0,0 +1,63 @@
+package com.tosh.reactivespring.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@RunWith(SpringRunner.class)
+@WebFluxTest
+class FluxAndMonoControllerTest {
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void flux_approach1(){
+        Flux<Integer> integerFlux = webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFlux)
+                .expectSubscription()
+                .expectNext(1,2,3,4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void flux_approach2(){
+       webTestClient.get().uri("/flux")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Integer.class)
+                .hasSize(4);
+    }
+
+    @Test
+    public void flux_approach3(){
+        List<Integer> expectedList = Arrays.asList(1,2,3,4);
+        EntityExchangeResult<List<Integer>> entityExchangeResult =webTestClient.get().uri("/flux")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Integer.class)
+                .returnResult();
+
+        assertEquals(expectedList, entityExchangeResult.getResponseBody());
+    }
+
+}
